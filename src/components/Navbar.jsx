64ae21da676cf5fe8089488a6249d155407468e1
Kiran Bhoi <bhoikiran241@gmail.com>
2026-01-07 17:41:53 +0530

    all changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,46 @@
 import { Link } from "react-router-dom";
+import { useState ,  useEffect} from "react";
 import Optenix from "../assets/Optenix.png";
 import "../style/Navbar.css";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+   // Handle scroll for color change
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) setScrolled(true);
+      else setScrolled(false);
+  };
+    window.addEventListener("scroll", handleScroll);
+  return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
+
   return (
-    <header className="navbar">
+    <header className={`navbar ${scrolled ? "scrolled" : ""}`}>
       <div className="navbar-container">
 
         {/* Logo */}
-        <Link to="/" className="logo">
+        <Link to="/" className="logo" onClick={() => setMenuOpen(false)}>
           <img src={Optenix} alt="Optenix Logo" />
         </Link>
 
-        {/* Navigation Links */}
-        <nav className="nav-links">
+        {/* Hamburger */}
+        <button
+          className={`menu-toggle ${menuOpen ? "active" : ""}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+          aria-label="Toggle menu"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        {/* Navigation */}
+        <nav className={`nav-links ${menuOpen ? "open" : ""}`}>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/shop">Shop</Link>
